9d8abf172d6becb5b981322f9f72dfba4831b721
Destiny <clara.rodriguez.a@hotmail.com>
2021-09-28 17:29:54 -0300

    Actividad 9

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
+/*
+Sugerencia: Es posible asociar más de un evento a un elemento y se pueden emplear funciòn comunes, 
+anónimas y arrow para los manejadores de eventos.
 
+>> Consigna: Asociar eventos que buscamos controlar sobre los elementos de la interfaz de nuestro 
+simulador
+
+>>Ejemplo:
+-Cuando el usuario selecciona un producto, se determina el id del seleccionado y se realiza una salida 
+en confirmación.
+-Capturar la tecla ENTER para confirmar alguna acción.
+
+*/
 class Producto {
     constructor(id, nombre, precio) {
         this.id = id;
@@ -17,9 +29,21 @@ const productos = [
 for (const producto of productos){
     let divProducto=document.createElement('div');    
     divProducto.innerHTML=`<h3>Nombre: ${producto.nombre}</h3>
-                            <p>ID: ${producto.id}</p>
-                            <p>Precio: ${producto.precio}</p>`;
+                            <p>Precio: ${producto.precio}</p>
+                            <button id='${producto.id}' class = 'btnCarrito'>Añadir al Carrito</button>`;
 
     document.body.appendChild(divProducto);
 }
 
+let botones=document.getElementsByClassName('btnCarrito');
+console.log(botones);
+
+for (const boton of botones) {
+        boton.addEventListener('click',Selection);
+        
+}
+
+function Selection(){
+    let seleccion= productos.find(producto => producto.id == this.id);
+    console.log('El id del producto seleccionado es '+seleccion.id);
+}   
